0db7c7278b0ab5dc30d38b533244821d872b42ed
yeasin <ya@ziotsolutions.com>
2021-09-23 15:12:49 +0600

    CQRS - Set Infrastructure.Persistence IOC in Startup.cs of API

--- src/CQRS.CleanArchitecture.Starter.Api/Startup.cs
+++ src/CQRS.CleanArchitecture.Starter.Api/Startup.cs
@@ -1,24 +1,27 @@
+using CQRS.CleanArchitecture.Starter.Infrastructure.Persistence;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace CQRS.CleanArchitecture.Starter.Api
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        public Startup(IConfiguration configuration)
         {
+            Configuration = configuration;
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddInfrastrcutureLayerServices(Configuration);
+        }
+
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
