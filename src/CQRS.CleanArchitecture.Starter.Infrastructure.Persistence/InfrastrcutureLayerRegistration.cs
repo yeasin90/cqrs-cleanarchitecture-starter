2d027d1f7a9051cae9d4a70e8d047d5415575b69
yeasin <ya@ziotsolutions.com>
2021-09-23 13:57:35 +0600

    CQRS - Added IoC for Infrastructure layer registration

---
+++ src/CQRS.CleanArchitecture.Starter.Infrastructure.Persistence/InfrastrcutureLayerRegistration.cs
@@ -0,0 +1,25 @@
+﻿using CQRS.CleanArchitecture.Starter.Core.Application.Contracts.Persistence;
+using CQRS.CleanArchitecture.Starter.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CQRS.CleanArchitecture.Starter.Infrastructure.Persistence
+{
+    public static class InfrastrcutureLayerRegistration
+    {
+        public static IServiceCollection AddInfrastrcutureLayerServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<CqrsStarterDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("CqrsStarterConnectionString")));
+
+            services.AddScoped(typeof(IAsyncRepository<>), typeof(BaseRepository<>));
+
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IEventRepository, EventRepository>();
+            services.AddScoped<IOrderRepository, OrderRepository>();
+
+            return services;
+        }
+    }
+}
