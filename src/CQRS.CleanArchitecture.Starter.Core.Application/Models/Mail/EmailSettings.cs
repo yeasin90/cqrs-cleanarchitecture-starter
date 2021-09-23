07e3b039c4250cc986d5055b1282c909e83caa42
Yeasin Abedin <abedinyeasin@gmail.com>
2021-09-23 23:59:50 +0600

    CQRS - Comment on Email and EmailSettings

--- src/CQRS.CleanArchitecture.Starter.Core.Application/Models/Mail/EmailSettings.cs
+++ src/CQRS.CleanArchitecture.Starter.Core.Application/Models/Mail/EmailSettings.cs
@@ -1,5 +1,8 @@
 ﻿namespace CQRS.CleanArchitecture.Starter.Core.Application.Models.Mail
 {
+    // Note: this class is NOT placed under CQRS.CleanArchitecture.Starter.Core.Domain project
+    // It's because, this is not a domain class
+    // This is just a configuration class used by Email service
     public class EmailSettings
     {
         public string ApiKey { get; set; }
