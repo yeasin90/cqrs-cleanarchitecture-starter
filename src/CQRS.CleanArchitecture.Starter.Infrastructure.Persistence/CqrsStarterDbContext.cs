4f1ee0a5a727d75b235a0b2c5f8675b178332c19
yeasin <ya@ziotsolutions.com>
2021-09-23 13:19:30 +0600

    CQRS - Added entity references in CqrsStarterDbContext class

--- src/CQRS.CleanArchitecture.Starter.Infrastructure.Persistence/CqrsStarterDbContext.cs
+++ src/CQRS.CleanArchitecture.Starter.Infrastructure.Persistence/CqrsStarterDbContext.cs
@@ -1,6 +1,17 @@
-﻿namespace CQRS.CleanArchitecture.Starter.Infrastructure.Persistence
+﻿using CQRS.CleanArchitecture.Starter.Core.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CQRS.CleanArchitecture.Starter.Infrastructure.Persistence
 {
-    public class CqrsStarterDbContext
+    public class CqrsStarterDbContext : DbContext
     {
+        public CqrsStarterDbContext(DbContextOptions<CqrsStarterDbContext> options)
+           : base(options)
+        {
+        }
+
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Order> Orders { get; set; }
     }
 }
