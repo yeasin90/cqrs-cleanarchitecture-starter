a52dc0f266c40788a0aad218edecf2d22f74387c
yeasin <ya@ziotsolutions.com>
2021-09-23 12:41:11 +0600

    CQRS - Added domain entities under 'Core' layer

---
+++ src/CQRS.CleanArchitecture.Starter.Core.Domain/Entities/Category.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CQRS.CleanArchitecture.Starter.Core.Domain.Entities
+{
+    public class Category
+    {
+        public Guid CategoryId { get; set; }
+        public string Name { get; set; }
+        public ICollection<Event> Events { get; set; }
+    }
+}
---
+++ src/CQRS.CleanArchitecture.Starter.Core.Domain/Entities/Order.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CQRS.CleanArchitecture.Starter.Core.Domain.Entities
+{
+    public class Order
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public int OrderTotal { get; set; }
+        public DateTime OrderPlaced { get; set; }
+        public bool OrderPaid { get; set; }
+    }
+}
