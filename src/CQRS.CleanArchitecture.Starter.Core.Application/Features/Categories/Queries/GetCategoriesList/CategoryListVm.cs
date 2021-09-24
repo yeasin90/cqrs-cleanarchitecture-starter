59298ec4dd68f313010df3844c994cb2c7e5852f
Yeasin Abedin <abedinyeasin@gmail.com>
2021-09-24 22:05:49 +0600

    CQRS - Added Queries for Categories feature

---
+++ src/CQRS.CleanArchitecture.Starter.Core.Application/Features/Categories/Queries/GetCategoriesList/CategoryListVm.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace CQRS.CleanArchitecture.Starter.Core.Application.Features.Categories.Queries.GetCategoriesList
+{
+    public class CategoryListVm
+    {
+        public Guid CategoryId { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ src/CQRS.CleanArchitecture.Starter.Core.Application/Features/Categories/Queries/GetCategoriesList/GetCategoriesListQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using CQRS.CleanArchitecture.Starter.Core.Application.Contracts.Persistence;
+using CQRS.CleanArchitecture.Starter.Core.Domain.Entities;
+using MediatR;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CQRS.CleanArchitecture.Starter.Core.Application.Features.Categories.Queries.GetCategoriesList
+{
+    public class GetCategoriesListQueryHandler : IRequestHandler<GetCategoriesListQuery, List<CategoryListVm>>
+    {
+        private readonly IAsyncRepository<Category> _categoryRepository;
+        private readonly IMapper _mapper;
+
+        public GetCategoriesListQueryHandler(IMapper mapper, IAsyncRepository<Category> categoryRepository)
+        {
+            _mapper = mapper;
+            _categoryRepository = categoryRepository;
+        }
+
+        public async Task<List<CategoryListVm>> Handle(GetCategoriesListQuery request, CancellationToken cancellationToken)
+        {
+            var allCategories = (await _categoryRepository.ListAllAsync()).OrderBy(x => x.Name);
+            return _mapper.Map<List<CategoryListVm>>(allCategories);
+        }
+    }
+}
