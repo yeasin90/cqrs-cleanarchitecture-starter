2b79f5fec0ef9e17915c85d8128e6c9418ded52d
yeasin <ya@ziotsolutions.com>
2021-09-24 11:55:20 +0600

    CQRS - Added IoC for Infrastructure mail class library

---
+++ src/CQRS.CleanArchitecture.Starter.Infrastructure.Infrastructure/InfrastructureServiceRegistration.cs
@@ -0,0 +1,21 @@
+﻿using CQRS.CleanArchitecture.Starter.Core.Application.Contracts.Infrastructure;
+using CQRS.CleanArchitecture.Starter.Core.Application.Models.Mail;
+using CQRS.CleanArchitecture.Starter.Infrastructure.Infrastructure.Mail;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CQRS.CleanArchitecture.Starter.Infrastructure.Infrastructure
+{
+    public static class InfrastructureServiceRegistration
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<EmailSettings>(configuration.GetSection("EmailSettings"));
+
+            services.AddTransient<IEmailService, EmailService>();
+
+            return services;
+        }
+    }
+}
