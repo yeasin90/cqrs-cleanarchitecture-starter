22e205dd99d534056d81c788bc6a0a83ef210b49
Yeasin Abedin <abedinyeasin@gmail.com>
2021-09-24 21:52:15 +0600

    CQRS - Added FluentValidation handlers

--- src/CQRS.CleanArchitecture.Starter.Core.Application/Features/Categories/Commands/CreateCateogry/CreateCategoryCommandHandler.cs
+++ src/CQRS.CleanArchitecture.Starter.Core.Application/Features/Categories/Commands/CreateCateogry/CreateCategoryCommandHandler.cs
@@ -23,6 +23,18 @@ namespace CQRS.CleanArchitecture.Starter.Core.Application.Features.Categories.Co
         {
             var createCategoryCommandResponse = new CreateCategoryCommandResponse();
 
+            var validator = new CreateCategoryCommandValidator();
+            var validationResult = await validator.ValidateAsync(request);
+
+            if (validationResult.Errors.Count > 0)
+            {
+                createCategoryCommandResponse.Success = false;
+                createCategoryCommandResponse.ValidationErrors = new List<string>();
+                foreach (var error in validationResult.Errors)
+                {
+                    createCategoryCommandResponse.ValidationErrors.Add(error.ErrorMessage);
+                }
+            }
             if (createCategoryCommandResponse.Success)
             {
                 var category = new Category() { Name = request.Name };
