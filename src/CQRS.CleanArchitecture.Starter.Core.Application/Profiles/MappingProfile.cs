6b267a5d745a83da204fcf8e42939f0675dc83f0
Yeasin Abedin <abedinyeasin@gmail.com>
2021-09-24 22:50:07 +0600

    CQRS - Added remaing object mappers in AutoMapper

--- src/CQRS.CleanArchitecture.Starter.Core.Application/Profiles/MappingProfile.cs
+++ src/CQRS.CleanArchitecture.Starter.Core.Application/Profiles/MappingProfile.cs
@@ -1,5 +1,13 @@
 ﻿using AutoMapper;
 using CQRS.CleanArchitecture.Starter.Core.Application.Features.Categories.Commands.CreateCateogry;
+using CQRS.CleanArchitecture.Starter.Core.Application.Features.Categories.Queries.GetCategoriesList;
+using CQRS.CleanArchitecture.Starter.Core.Application.Features.Categories.Queries.GetCategoriesListWithEvents;
+using CQRS.CleanArchitecture.Starter.Core.Application.Features.Events.Commands.CreateEvent;
+using CQRS.CleanArchitecture.Starter.Core.Application.Features.Events.Commands.UpdateEvent;
+using CQRS.CleanArchitecture.Starter.Core.Application.Features.Events.Queries.GetEventDetail;
+using CQRS.CleanArchitecture.Starter.Core.Application.Features.Events.Queries.GetEventsExport;
+using CQRS.CleanArchitecture.Starter.Core.Application.Features.Events.Queries.GetEventsList;
+using CQRS.CleanArchitecture.Starter.Core.Application.Features.Orders.Queries.GetOrdersForMonth;
 using CQRS.CleanArchitecture.Starter.Core.Domain.Entities;
 
 namespace CQRS.CleanArchitecture.Starter.Core.Application.Profiles
@@ -8,8 +16,20 @@ namespace CQRS.CleanArchitecture.Starter.Core.Application.Profiles
     {
         public MappingProfile()
         {
+            CreateMap<Event, EventListVm>().ReverseMap();
+            CreateMap<Event, CreateEventCommand>().ReverseMap();
+            CreateMap<Event, UpdateEventCommand>().ReverseMap();
+            CreateMap<Event, EventDetailVm>().ReverseMap();
+            CreateMap<Event, CategoryEventDto>().ReverseMap();
+            CreateMap<Event, EventExportDto>().ReverseMap();
+
+            CreateMap<Category, CategoryDto>();
+            CreateMap<Category, CategoryListVm>();
+            CreateMap<Category, CategoryEventListVm>();
             CreateMap<Category, CreateCategoryCommand>();
             CreateMap<Category, CreateCategoryDto>();
+
+            CreateMap<Order, OrdersForMonthDto>();
         }
     }
 }
