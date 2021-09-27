c2762d293c8f50e5a77397f91a01704c3ea41b95
yeasin <ya@ziotsolutions.com>
2021-09-27 18:14:26 +0600

    CQRS - Add Swagger support

--- src/CQRS.CleanArchitecture.Starter.Api/Startup.cs
+++ src/CQRS.CleanArchitecture.Starter.Api/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using CQRS.CleanArchitecture.Starter.Api.Utility;
 
 namespace CQRS.CleanArchitecture.Starter.Api
 {
@@ -21,15 +22,8 @@ namespace CQRS.CleanArchitecture.Starter.Api
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddApiVersioning(config =>
-            {
-                // Specify the default API Version as 1.0
-                config.DefaultApiVersion = new ApiVersion(1, 0);
-                // If the client hasn't specified the API version in the request, use the default API version number 
-                config.AssumeDefaultVersionWhenUnspecified = true;
-                // Advertise the API versions supported for the particular endpoint
-                config.ReportApiVersions = true;
-            });
+            AddSwagger(services);
+            AddApiVersioning(services);
 
             services.AddApplicationServices();
             services.AddInfrastrcuturePersistenceServices(Configuration);
@@ -43,6 +37,33 @@ namespace CQRS.CleanArchitecture.Starter.Api
             }); 
         }
 
+        private void AddSwagger(IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "CQRS Starter"
+                });
+
+                c.OperationFilter<FileResultContentTypeOperationFilter>();
+            });
+        }
+
+        private void AddApiVersioning(IServiceCollection services)
+        {
+            services.AddApiVersioning(config =>
+            {
+                // Specify the default API Version as 1.0
+                config.DefaultApiVersion = new ApiVersion(1, 0);
+                // If the client hasn't specified the API version in the request, use the default API version number 
+                config.AssumeDefaultVersionWhenUnspecified = true;
+                // Advertise the API versions supported for the particular endpoint
+                config.ReportApiVersions = true;
+            });
+        }
+
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -53,6 +74,11 @@ namespace CQRS.CleanArchitecture.Starter.Api
             // Check: https://www.appmarq.com/public/security,1043066,Always-use-HTTPS-Redirection-Middleware-and-HSTS-Middleware-in-your-ASPNET-Core-application
             app.UseHttpsRedirection();
             app.UseRouting();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "CQRS Starter");
+            });
             app.UseCors("Open");
             app.UseEndpoints(endpoints =>
             {
